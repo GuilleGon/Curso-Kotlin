460a8ebf131e34a79a8d8ab464723b4af856ceac
Guillermo <nicomano021@gmail.com>
2023-11-27 17:03:25 -0300

    Kotlin, Hola mundo y tipos de datos

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+const val PI = 3.1416
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var diner = 10
+    diner = 5
+
+    println("dinero: $diner")
+
+    val name = "Guillermo"
+    println("nombre: $name")
+
+    println(PI)
+}
