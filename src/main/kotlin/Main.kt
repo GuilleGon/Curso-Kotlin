ed927be1964ba7243367d21ca12b5bec6938ff0e
Guillermo <nicomano021@gmail.com>
2023-11-28 16:30:38 -0300

    ciclos & TryCatch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,13 @@
-@file:Suppress("UNUSED_EXPRESSION")
-
 const val PI = 3.1416
 fun main(args: Array<String>) {
-    val nombre = "Guillermo"
+    val listaDeFrutas = listOf("Manzana", "Pera", "Frutilla", "Durazno")
+    for (fruta in listaDeFrutas)
+        println("Fruta: $fruta")
 
-    if(nombre.isNotEmpty()){
-        println("El largo de nuestra variable es: ${nombre.length}")
-    }else {
-        println("La variable está vacia")
-    }
-
-    val mensaje : String = if (nombre.length > 5)
-        "El nombre es largo"
-    else "el nombre es corto"
-
-    println(mensaje)
-
-    val nombreColor = "Amarillo"
-
-    when(nombreColor){
-        "Amarillo" -> println("Amarillo como el sol")
-        "Rojo" -> println("Rojo como la frutilla")
-        "Azul" -> println("Azul como el cielo")
-    }
-
-    val num = 350
-    when(num){
-        in 100..250 -> println("Tu numero está entre 100 y 250")
-        in 251..350 -> println("Tu numero está entre 251 y 350")
-        in 351..450 -> println("Tu numero esta entre 351 y 450")
-    }
+    var nombre: String? = null
+   try {
+       nombre!!.length
+   }catch (excepcion: NullPointerException){
+       println("Ah ocurrido un error")
+   }
 }
