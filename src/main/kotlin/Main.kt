6965eb83d65705f2c5e1ff213b188caf1fc15204
Guillermo <nicomano021@gmail.com>
2023-11-30 17:57:38 -0300

    funciones

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
+
 fun main(args: Array<String>) {
-    val vocalesRepetidas = setOf("a","e","i","o","u","a","e","i","o","u")
-    println(vocalesRepetidas)
+    val fraseAleatoria = "Pablito clavo un clavito".randomCase()
+    imprimirFrase(fraseAleatoria)
+}
 
-    val numeroFavoritos = mutableSetOf(1,2,3,4)
-    println(numeroFavoritos)
-    numeroFavoritos.add(5)
-    numeroFavoritos.add(5)
-    println(numeroFavoritos)
+fun imprimirFrase(frase: String): Unit {
+    println("Fase: $frase")
+}
 
-    numeroFavoritos.remove(5)
-    println(numeroFavoritos)
-
-    val valorDelSet = numeroFavoritos.firstOrNull{ numero -> numero > 2}
-    println(valorDelSet)
+fun String.randomCase(): String {
+    val numeroAleatorio = 0..99
+    val resultadoAleatorio = numeroAleatorio.random()
+    return if (resultadoAleatorio.rem(2) == 0) {
+        this.toUpperCase()
+    } else {
+        this.toLowerCase()
+    }
 }
