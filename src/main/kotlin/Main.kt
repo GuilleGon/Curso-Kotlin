55660518b748f1394b33e3b1aa60c30d108a5548
Guillermo <nicomano021@gmail.com>
2023-12-01 16:29:37 -0300

    Run

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
 fun main(args: Array<String>) {
-    val colores = listOf("Azul", "Amarillo", "Rojo")
-    with(colores){
-        println("Nuestros colores son $this")
-        println("Esta lista tiene una cantidad de colores $size")
-    }
+   val moviles = mutableListOf("Motorola G200 5G", "Samsung A72","Red Magic 8 pro")
+       .run{
+           removeIf{ movil -> movil.contains("Motorola")}
+           this
+       }
 
+    println(moviles)
 }
